ed3c3c25727527f3ce08ac56309bedefa438bb63
Mithilesh12@crowdbotics.com <Mithilesh12@crowdbotics.com>
2022-08-18 00:47:40 +0530

    Crowdbotics Studio: Update app screens

--- screens/Untitled5/index.js
+++ screens/Untitled5/index.js
@@ -1,14 +1,47 @@
+import { ActivityIndicator } from "react-native";
+import { Slider } from "react-native-elements";
+import { CheckBox } from "react-native-elements";
+import { TextInput } from "react-native";
 import React from "react";
 import { View } from "react-native";
 
 const Untitled5 = () => {
   return <View style={{
-    backgroundColor: '#f0f0f1',
+    backgroundColor: "#3a3ad0",
     padding: 10,
-    position: 'relative',
-    height: '100%'
+    position: "relative",
+    height: "100%"
   }}>
-      </View>;
+      <TextInput style={{
+      left: 125,
+      top: 65,
+      position: "absolute",
+      backgroundColor: "#911818",
+      borderColor: "#cccccc",
+      width: 252,
+      height: 30,
+      transform: "rotate(180deg)"
+    }}></TextInput><CheckBox title="correct box" style={{
+      left: 93,
+      top: 138,
+      position: "absolute"
+    }} size={80} checked={false}></CheckBox><Slider thumbStyle={{
+      height: 20,
+      width: 20
+    }} thumbTintColor="#0000FF" maximumValue={1} minimumValue={0} style={{
+      left: 131,
+      top: 241,
+      position: "absolute",
+      width: 150,
+      height: 80
+    }}></Slider><ActivityIndicator style={{
+      left: 165,
+      top: 304,
+      position: "absolute",
+      width: 50,
+      height: 50,
+      backgroundColor: "#12b219"
+    }}></ActivityIndicator></View>;
 };
 
 export default Untitled5;
